da38768b16aba445c3ab4e1f621d22e31c69756d
morrys <lorenzo.digiacomo85@gmail.com>
2020-05-28 20:38:37 +0200

    fix format:ci

--- src/relay/queryFieldFragment.graphql.ts
+++ src/relay/queryFieldFragment.graphql.ts
@@ -1,43 +1,41 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { ReaderFragment } from "relay-runtime";
-import { FragmentRefs } from "relay-runtime";
+import { ReaderFragment } from 'relay-runtime';
+import { FragmentRefs } from 'relay-runtime';
 export type queryFieldFragment = {
     readonly id: string;
     readonly check: string | null;
-    readonly " $refType": "queryFieldFragment";
+    readonly ' $refType': 'queryFieldFragment';
 };
 export type queryFieldFragment$data = queryFieldFragment;
 export type queryFieldFragment$key = {
-    readonly " $data"?: queryFieldFragment$data;
-    readonly " $fragmentRefs": FragmentRefs<"queryFieldFragment">;
+    readonly ' $data'?: queryFieldFragment$data;
+    readonly ' $fragmentRefs': FragmentRefs<'queryFieldFragment'>;
 };
 
-
-
 const node: ReaderFragment = {
-  "kind": "Fragment",
-  "name": "queryFieldFragment",
-  "type": "Entry",
-  "metadata": null,
-  "argumentDefinitions": [],
-  "selections": [
-    {
-      "kind": "ScalarField",
-      "alias": null,
-      "name": "id",
-      "args": null,
-      "storageKey": null
-    },
-    {
-      "kind": "ScalarField",
-      "alias": null,
-      "name": "check",
-      "args": null,
-      "storageKey": null
-    }
-  ]
+    kind: 'Fragment',
+    name: 'queryFieldFragment',
+    type: 'Entry',
+    metadata: null,
+    argumentDefinitions: [],
+    selections: [
+        {
+            kind: 'ScalarField',
+            alias: null,
+            name: 'id',
+            args: null,
+            storageKey: null,
+        },
+        {
+            kind: 'ScalarField',
+            alias: null,
+            name: 'check',
+            args: null,
+            storageKey: null,
+        },
+    ],
 };
 (node as any).hash = 'd5b0953bdeb412925ee13444a6be6082';
 export default node;
--- src/relay/queryFieldQuery.graphql.ts
+++ src/relay/queryFieldQuery.graphql.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 /* @relayHash dcfff3d9ea8d483cec6a2916d3f744cc */
 
-import { ConcreteRequest } from "relay-runtime";
+import { ConcreteRequest } from 'relay-runtime';
 export type queryFieldQueryVariables = {};
 export type queryFieldQueryResponse = {
     readonly form: {
@@ -20,8 +20,6 @@ export type queryFieldQuery = {
     readonly variables: queryFieldQueryVariables;
 };
 
-
-
 /*
 query queryFieldQuery {
   form {
@@ -36,90 +34,91 @@ query queryFieldQuery {
 }
 */
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "kind": "LinkedField",
-    "alias": null,
-    "name": "form",
-    "storageKey": null,
-    "args": null,
-    "concreteType": "EntryForm",
-    "plural": false,
-    "selections": [
-      {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "entries",
-        "storageKey": null,
-        "args": null,
-        "concreteType": "Entry",
-        "plural": true,
-        "selections": [
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "id",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "key",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "value",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "check",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "error",
-            "args": null,
-            "storageKey": null
-          }
-        ]
-      }
-    ]
-  }
-];
-return {
-  "kind": "Request",
-  "fragment": {
-    "kind": "Fragment",
-    "name": "queryFieldQuery",
-    "type": "Query",
-    "metadata": null,
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "operation": {
-    "kind": "Operation",
-    "name": "queryFieldQuery",
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "params": {
-    "operationKind": "query",
-    "name": "queryFieldQuery",
-    "id": null,
-    "text": "query queryFieldQuery {\n  form {\n    entries {\n      id\n      key\n      value\n      check\n      error\n    }\n  }\n}\n",
-    "metadata": {}
-  }
-};
+const node: ConcreteRequest = (function() {
+    var v0 = [
+        {
+            kind: 'LinkedField',
+            alias: null,
+            name: 'form',
+            storageKey: null,
+            args: null,
+            concreteType: 'EntryForm',
+            plural: false,
+            selections: [
+                {
+                    kind: 'LinkedField',
+                    alias: null,
+                    name: 'entries',
+                    storageKey: null,
+                    args: null,
+                    concreteType: 'Entry',
+                    plural: true,
+                    selections: [
+                        {
+                            kind: 'ScalarField',
+                            alias: null,
+                            name: 'id',
+                            args: null,
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'ScalarField',
+                            alias: null,
+                            name: 'key',
+                            args: null,
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'ScalarField',
+                            alias: null,
+                            name: 'value',
+                            args: null,
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'ScalarField',
+                            alias: null,
+                            name: 'check',
+                            args: null,
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'ScalarField',
+                            alias: null,
+                            name: 'error',
+                            args: null,
+                            storageKey: null,
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
+    return {
+        kind: 'Request',
+        fragment: {
+            kind: 'Fragment',
+            name: 'queryFieldQuery',
+            type: 'Query',
+            metadata: null,
+            argumentDefinitions: [],
+            selections: v0 /*: any*/,
+        },
+        operation: {
+            kind: 'Operation',
+            name: 'queryFieldQuery',
+            argumentDefinitions: [],
+            selections: v0 /*: any*/,
+        },
+        params: {
+            operationKind: 'query',
+            name: 'queryFieldQuery',
+            id: null,
+            text:
+                'query queryFieldQuery {\n  form {\n    entries {\n      id\n      key\n      value\n      check\n      error\n    }\n  }\n}\n',
+            metadata: {},
+        },
+    };
 })();
 (node as any).hash = 'f9fd20a80a839dfb51206c305cbb1657';
 export default node;
